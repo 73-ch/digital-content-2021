951bff013a695fea22ed113ef8673c05146ad288
Jun Nishimura <n.junjun0303@gmail.com>
2021-11-28 11:24:17 +0900

    sass変数整備

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
   <router-view/>
 </template>
 
-<style>
+<style lang="scss">
 * {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+}
+
+#app {
+  color: $color__default;
+  font-family: $font-family__default;
 }
 </style>
