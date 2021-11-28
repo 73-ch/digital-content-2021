9e87ef5918a97834cb3b0df85a0b6b4c8673a17b
Jun Nishimura <n.junjun0303@gmail.com>
2021-11-28 11:24:53 +0900

    add home footer

---
+++ src/components/HomeFooter.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home-footer">
+    <div class="home-footer__btns">
+      <div class="home-footer__btns-btn">
+        <button>Member</button>
+      </div>
+      <div class="home-footer__btns-btn">
+        <button class="home-footer__btns-label">Producer</button>
+        <button>Namikawa Kosaku</button>
+      </div>
+      <div class="home-footer__btns-btn">
+        <button class="home-footer__btns-label">Teacher</button>
+        <button>Ochiai Yoichi</button>
+      </div>
+      <div class="home-footer__btns-btn">
+        <button>Other members involved in this project</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.home-footer {
+  height: $height__home-footer;
+  padding: 0 2rem;
+  &__btns {
+    margin-left: $width__home-contents-side;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    height: 100%;
+    &-btn {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      button {
+        width: 180px;
+        height: 100%;
+        margin: 0.1rem;
+      }
+    }
+    &-label {
+      pointer-events: none;
+    }
+  }
+}
+</style>
