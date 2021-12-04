7c16727b1a37ca53299c335437bff078f0c33f6f
tai5863 <60917493+tai5863@users.noreply.github.com>
2021-12-04 20:10:05 +0900

    Calibrationコンポーネントの追加とポジションのfix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,47 @@
 <template>
- <p>home</p>
+  <div id="home" :style="{'background-image': `url(${bgImage})`}">
+    <home-header />
+    <home-contents />
+    <home-footer />
+  </div>
+  <!-- 外に書く -->
+  <calibration />
 </template>
 
 <script>
+import { computed } from 'vue'
+import HomeHeader from '@/components/HomeHeader.vue'
+import HomeContents from '@/components/HomeContents.vue'
+import HomeFooter from '@/components/HomeFooter.vue'
+import Calibration from '@/components/Calibration.vue'
 export default {
-  name: "Home"
+  components: {
+    HomeHeader,
+    HomeContents,
+    HomeFooter,
+    Calibration
+  },
+  setup() {
+    const bgImage = computed(() => require("@/assets/images/background.png"))
+
+    return { bgImage }
+  }
 }
 </script>
 
 <style scoped>
-p {
-  color: black;
+#home {
+  width: 100vw;
+  height: 100vh;
+  padding: 1.6rem;
+  background-repeat: repeat;
+  background-size: cover;
+  background-color: #777;
+
+  position: fixed;
+}
+calibration {
+  visibility: visible; /* キャリブレーション用 */
+  /* visibility: visible; 本番用 */
 }
 </style>
