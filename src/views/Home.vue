318dde385da768cd748641e09ad42eb229dec2d4
nami <cigarbox.nami@gmail.com>
2021-12-04 16:48:05 +0900

    renamed Home -> Dummy, created Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,15 @@
 <template>
-  <div class="home" :style="{'background-image': `url(${bgImage})`}">
-    <home-header />
-    <home-contents />
-    <home-footer />
-  </div>
+ <p>home</p>
 </template>
 
 <script>
-import { computed } from 'vue'
-import HomeHeader from '@/components/HomeHeader.vue'
-import HomeContents from '@/components/HomeContents.vue'
-import HomeFooter from '@/components/HomeFooter.vue'
 export default {
-  components: {
-    HomeHeader,
-    HomeContents,
-    HomeFooter
-  },
-  setup() {
-    const bgImage = computed(() => require("@/assets/images/background.png"))
-
-    return { bgImage }
-  }
+  name: "Home"
 }
 </script>
 
-<style scoped lang="scss">
-.home {
-  width: 100vw;
-  height: 100vh;
-  padding: 1.6rem;
-  background-repeat: repeat;
-  background-size: cover;
-  background-color: #777;
+<style scoped>
+p {
+  color: black;
 }
 </style>
