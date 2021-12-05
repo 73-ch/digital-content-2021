6c7f65c69389c0feb222105a8789b11138e23a6c
kzkymur <kamilu213@icloud.com>
2021-12-05 11:04:44 +0900

    とりあえずカルーセル実装

---
+++ src/components/Arrow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    direction: String,
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  width: 100px;
+  height: 100%;
+  background: red;
+}
+</style>
