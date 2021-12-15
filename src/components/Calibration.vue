303d542572b5557d567759fa04380e534ded3226
tai5863 <60917493+tai5863@users.noreply.github.com>
2021-12-16 04:25:41 +0900

    Revert "delete perspective"
    This reverts commit 7cf3c66cf29369c1b93fe4ba5be9a8f29b3624d8.

--- src/components/Calibration.vue
+++ src/components/Calibration.vue
@@ -17,6 +17,22 @@
         />
       </div>
       <p class="param">{{ computedTransform }}</p>
+      <div id="perspective" class="param">
+        <div>
+          <label for="perspective">Perspective: </label>
+          <span id="val_perspective">{{ perspective }}</span
+          ><span>px</span>
+          <span><input type="text" v-model="setPerspective" class="set_param"/></span>
+        </div>
+        <input
+          type="range"
+          id="range_perspective"
+          min="0"
+          max="1500"
+          step="1"
+          v-model="perspective"
+        />
+      </div>
       <div id="translate_x" class="param">
         <div>
           <label for="translate_x">Translate X: </label>
@@ -156,6 +172,7 @@ export default {
       targetElement: null,
       parentElement: null,
       params: {},
+      perspective: 600,
       translateX: 0,
       translateY: 0,
       translateZ: 0,
@@ -175,6 +192,14 @@ export default {
       transformString += `rotate3d(0, 0, 1,${this.rotateZ}deg) `;
       transformString += `scale3d(${this.scaleX}, ${this.scaleY}, 1 )`;
       return transformString;
+    },
+    setPerspective: {
+      get() {
+        return this.perspective
+      },
+      set(value) {
+        this.perspective = value
+      }
     },
     setTranslateX: {
       get() {
@@ -245,6 +270,9 @@ export default {
     computedTransform(value) {
       this.targetElement.style.transform = value;
     },
+    perspective(value) {
+      this.parentElement.style.perspective = value + "px";
+    },
   },
   methods: {
     calibration() {
@@ -252,6 +280,7 @@ export default {
       let transformJson = require("@/assets/calibration-data/" + this.targetId + ".json");
 
       if (transformJson) {
+        this.perspective = transformJson.perspective;
         this.translateX = transformJson.translateX;
         this.translateY = transformJson.translateY;
         this.translateZ = transformJson.translateZ;
@@ -276,7 +305,7 @@ export default {
       this.targetElement = targetElement;
       this.parentElement = targetElement.parentElement;
       this.parentElement.style.transformStyle = "preserve-3d";
-      this.parentElement.style.perspective = "600px";
+      this.parentElement.style.perspective = this.perspective + "px";
 
       // ターゲットのvisibilityを扱うためのエレメント
       let view = document.getElementById("checkbox_view");
@@ -307,6 +336,7 @@ export default {
 
       const data = {
         transformString: this.computedTransform,
+        perspective: this.perspective,
         translateX: this.translateX,
         translateY: this.translateY,
         translateZ: this.translateZ,
