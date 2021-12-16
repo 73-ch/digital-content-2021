809e63af6ea7a817c9e4620c8423f2a1f7c6cbae
nami <cigarbox.nami@gmail.com>
2021-12-16 11:23:31 +0900

    add youtube viewer

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Dummy from '@/views/Dummy.vue'
 import Home from "@/views/Home";
 import Concept from '@/views/Concept.vue'
 import Credit from '@/views/Credit.vue'
+import YoutubeViewer from "@/views/YoutubeViewer";
 
 const routes = [
   {
@@ -30,6 +31,12 @@ const routes = [
     path: '/credit',
     name: 'Credit',
     component: Credit
+  },
+  {
+    path: '/viewer',
+    name: 'Viewer',
+    component: YoutubeViewer,
+    props: true
   },
   {
     // それ以外はホーム画面に遷移
---
+++ src/views/YoutubeViewer.vue
@@ -0,0 +1,87 @@
+<template>
+  <main>
+    <section class="caption">
+      <h2>{{this.$route.query.number}}</h2>
+      <h1>{{this.$route.query.title}}</h1>
+      <h2>{{this.$route.query.creator}}</h2>
+    </section>
+    <section class="video">
+      <iframe id="youtube" :width="this.$route.query.width" :height="this.$route.query.height" :src="`https://www.youtube.com/embed/${this.$route.query.id}?autoplay=1&showinfo=0&rel=0&iv_load_policy=3&theme=light&color=white`" title="YouTube video player"
+          frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen></iframe>
+    </section>
+    <section class="control">
+      <button @click="close">閉じる</button>
+    </section>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: "YoutubeViewer",
+  mounted() {
+    const aspectRatio = (this.$route.query.width / this.$route.query.height) || 16/9;
+
+    window.addEventListener('resize', () => {
+      console.log("read", this.$refs.youtube);
+      const calcWidth = aspectRatio * window.innerHeight * 0.8;
+      const calcHeight = window.innerWidth / aspectRatio * 0.8;
+      const videoSize = calcWidth < window.innerWidth * 0.8 ? [calcWidth, window.innerHeight * 0.8] : [window.innerWidth * 0.8, calcHeight];
+
+      this.$refs.youtube.style.width = videoSize[0] + "px";
+      this.$refs.youtube.style.height = videoSize[1] + "px";
+    });
+  },
+  methods: {
+    close() {
+      window.close();
+    }
+  }
+}
+</script>
+
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  /*justify-content: center;*/
+  align-items: center;
+  flex-direction: column;
+}
+
+.caption {
+  width: 100%;
+  padding: 2.5vh 10vw;
+}
+
+h1 {
+  font-size: 50px;
+}
+h2 {
+  font-size: 25px;
+}
+
+h1,h2 {
+  color: black;
+  text-align: left;
+}
+
+.video {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70vw;
+  height: 39.375vw;
+}
+
+#youtube {
+  width: 70vw;
+  height: 39.375vw;
+}
+
+.control {
+  margin: 50px 0;
+}
+</style>
