5ad4c17d3c58d7767ab493095432e7fcf40c9686
nami <cigarbox.nami@gmail.com>
2022-02-19 11:31:49 +0900

    Exhibitionで動画をループ再生にする機能と作品ページの説明を追加

--- src/components/Exhibition.vue
+++ src/components/Exhibition.vue
@@ -2,7 +2,7 @@
   <div>
     <iframe
       class="gallary"
-      :src="`https://www.youtube.com/embed/${id}?controls=0&autoplay=1&mute=1`"
+      :src="`https://www.youtube.com/embed/${isLoop ?id.replace(/&.*/g, '') :  id}?controls=0&autoplay=1&mute=1${isLoop ? '&loop=1&playlist='+id : ''}`"
       title="YouTube video player"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -15,6 +15,11 @@
 export default {
   props: {
     id: String,
+  },
+  computed: {
+    isLoop() {
+      return this.id.indexOf('loop') > -1;
+    }
   }
 };
 </script>
